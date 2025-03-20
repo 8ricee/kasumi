c8340f00b770b44fa276fc73d014383a7df23e7f
Linh Tran <8ricee@gmail.com>
2025-03-21 06:50:27 +0700

    Update layout.tsx to add Vietnamese font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,11 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import { ThemeProvider } from "@/components/theme-provider"
+import { ThemeProvider } from "@/components/theme-provider";
+import { Inter } from "next/font/google";
+
+const inter = Inter({
+  subsets: ["latin", "vietnamese"],
+});
 
 export const metadata: Metadata = {
   title: "Kasumi",
@@ -19,12 +24,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en" suppressHydrationWarning className={inter.className}>
       <head />
       <body>
         <ThemeProvider
           attribute="class"
-          defaultTheme="system"
+          defaultTheme="dark"
           enableSystem
           disableTransitionOnChange
         >
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        "./src/**/*.{html,js,ts,jsx,tsx}", // Thay đổi đường dẫn nếu cần
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
